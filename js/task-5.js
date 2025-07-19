3bcb28ee9cc11a9bced7c52f3b05829e688eefb1
Umay Ece Mantar <umayece0@gmail.com>
2025-07-19 18:46:25 +0300

    task 4 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,12 @@
+const btn = document.querySelector('.change-color');
+const colorText = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
